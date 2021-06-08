7f20f9389eb2fc327bb4d419423054e2153c35ba
BuildTools <unconfigured@null.spigotmc.org>
2021-06-08 08:33:15 +0200

    message

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>UCH</h1>
-      <h2>ABC</h2>
-      <p>HELLLOOOOO</p>
+      <h1>Af Lukas og Simon</h1>
+      <h2>Benjamins Fanside</h2>
+      <p>Velkommen til Benjamins fanside her har vi nogle billeder af Benjamin</p>
     </div>
   )
 }
